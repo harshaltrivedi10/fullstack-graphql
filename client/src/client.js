b4ad4015201b7b7ca070e3dea1381e532a1a78b3
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-08-17 14:37:14 -0700

    Initial formatting completed.

--- client/src/client.js
+++ client/src/client.js
@@ -1,7 +1,7 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default

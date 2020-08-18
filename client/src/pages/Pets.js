1eaa6ff3fbc9c92460d330473087a93835cd4d4b
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-08-18 16:14:49 -0700

    Added query and mutation with logic of updating the cache.

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,57 @@ import PetsList from "../components/PetsList";
 import NewPetModal from "../components/NewPetModal";
 import Loader from "../components/Loader";
 
+const GET_PETS = gql`
+  query getPets {
+    pets {
+      name
+      type
+      img
+      id
+    }
+  }
+`;
+
+const ADD_NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
 export default function Pets() {
   const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(GET_PETS);
+
+  const [createPet, response] = useMutation(ADD_NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const data = cache.readQuery({ query: GET_PETS });
+      cache.writeQuery({
+        query: GET_PETS,
+        data: { pets: [addPet, ...data.pets] },
+      });
+    },
+  });
 
   const onSubmit = (input) => {
     setModal(false);
+    createPet({ variables: { newPet: input } });
   };
 
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
+  }
+
+  if (loading || response.loading) {
+    return <Loader />;
+  }
+
+  if (error || response.error) {
+    return <p>Error!</p>;
   }
 
   return (
@@ -30,7 +72,7 @@ export default function Pets() {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   );
